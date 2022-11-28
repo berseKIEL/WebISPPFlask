d3c44f372dcf7eb1f6b667dbe98b7cff2c304c88
BerseKIEL <exebarco280320@gmail.com>
2022-11-28 18:35:16 -0300

    se edita las config del repo privado

--- config/default.py
+++
@@ -1,7 +0,0 @@
-SECRET_KEY = 'key-secret'
-
-MYSQL_HOST = 'localhost'
-MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'root'
-MYSQL_DB = 'dbispp'
-MYSQL_PORT = 3306
---
+++ config/testing.py
@@ -0,0 +1,7 @@
+SECRET_KEY = 'testeodeapidepytohn1239ij12908jasdijd'
+
+MYSQL_HOST = 'bqjjhi4zjgxruq8i59y1-mysql.services.clever-cloud.com'
+MYSQL_USER = 'u6aqwg1u7v5mflqy'
+MYSQL_PASSWORD = 'dVYDi6R5oC4hg0UiVPL'
+MYSQL_DB = 'bqjjhi4zjgxruq8i59y1'
+MYSQL_PORT = 20707
