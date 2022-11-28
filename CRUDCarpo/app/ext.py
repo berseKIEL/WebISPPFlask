ce39fc1c099e38a19b9018615f927ad8eadeef88
BerseKIEL <exebarco280320@gmail.com>
2022-11-28 18:04:28 -0300

    reacomodo de estructura

--- CRUDCarpo/app/ext.py
+++
@@ -1,9 +0,0 @@
-from flask_cors import CORS
-from flask_mysqldb import MySQL
-from flask_wtf.csrf import CSRFProtect
-
-cors = CORS()
-
-db = MySQL()
-
-csrf = CSRFProtect()
--- CRUDCarpo/app/views/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, flash, request
-
-vistas = Blueprint("vistas", __name__)
-
-@vistas.route("/")
-def home():
-    return render_template("home.html")
